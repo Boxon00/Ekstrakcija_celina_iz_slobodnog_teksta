ade6b849fb0884ec4aeee3bf73104387e306df5f
Bojan Mitic <97413945+Boxon00@users.noreply.github.com>
2025-10-22 09:23:17 +0200

    Delete Projekat-Ekstrakcija celina iz slobodnog teksta/web_mining_project directory

--- Projekat-Ekstrakcija celina iz slobodnog teksta/web_mining_project/modules/embeddings.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-def create_embeddings(sentences):
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    embeddings = model.encode(sentences, show_progress_bar=True)
-    return embeddings
